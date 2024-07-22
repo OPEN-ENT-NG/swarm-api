91f09366b0ae3c1efab2bce7eb274780a95c3002
Simon Ledunois <simon.ledunois@cgi.com>
2024-07-22 14:50:55 +0200

    chore: create simple architecture and tests

---
+++ src/main/java/fr/cgi/learninghub/swarm/model/User.java
@@ -0,0 +1,4 @@
+package fr.cgi.learninghub.swarm.model;
+
+public record User(String name) {
+}
---
+++ src/test/java/fr/cgi/learninghub/swarm/resources/UserResourceTest.java
@@ -0,0 +1,30 @@
+package fr.cgi.learninghub.swarm.resources;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import io.quarkus.test.security.oidc.OidcSecurity;
+import io.quarkus.test.security.oidc.UserInfo;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class UserResourceTest {
+
+    @Test
+    @TestSecurity(user = "John DOE")
+    @OidcSecurity(
+            userinfo = {
+                    @UserInfo(key = "name", value = "John DOE"),
+            }
+    )
+
+    void testHelloEndpoint() {
+        given()
+                .when().get("/users/me")
+                .then()
+                .statusCode(200)
+                .body("name", is("John DOE"));
+    }
+}
