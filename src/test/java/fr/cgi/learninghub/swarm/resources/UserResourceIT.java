aede2b70a09f04a60d25cab52a34548b4a813ffb
Simon Ledunois <simon.ledunois@cgi.com>
2024-07-23 13:20:55 +0000

    chore: add ci

---
+++ src/test/java/fr/cgi/learninghub/swarm/resources/UserResourceIT.java
@@ -0,0 +1,7 @@
+package fr.cgi.learninghub.swarm.resources;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class UserResourceIT extends UserResourceTest {
+}
